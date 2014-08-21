64a55ca3a720040554f8d54f04189e8d4ffd21d3
Sean M. Sullivan <sean@barriquesoft.com>
2014-08-21 14:59:30 -0700

    Added failover script templates.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sean@pulselocker.com'
 license          'Apache 2.0'
 description      'Installs/Configures pgpool2-cookbook'
 long_description 'Installs/Configures pgpool2-cookbook'
-version          '1.0.0'
+version          '1.0.1'
 
 recipe 'pgpool2', 'Installs and configures pgpool-II with a simple configuration.'
 recipe 'pgpool2::install', 'Installs pgpool-II from source.'
