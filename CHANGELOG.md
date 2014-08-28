f0597af4bf9ea2b5159da6b18826482954f4a15a
Sean M. Sullivan <sean@barriquesoft.com>
2014-08-28 15:12:31 -0700

    Added unit test harness.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,14 @@ PGPOOL2-COOKBOOK CHANGELOG
 
 Be sure to view the README for implementation details!
 
+1.0.3
+-----
+- Added test harness for unit test coverage.
+
+1.0.2
+-----
+- Added dependency versions.
+
 1.0.1
 -----
 - Fixed bug for Debian systems.  Performs apt update before installing other packages.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sean@pulselocker.com'
 license          'Apache 2.0'
 description      'Installs/Configures pgpool2-cookbook'
 long_description 'Installs/Configures pgpool2-cookbook'
-version          '1.0.2'
+version          '1.0.3'
 
 recipe 'pgpool2', 'Installs and configures pgpool-II with a simple configuration.'
 recipe 'pgpool2::install', 'Installs pgpool-II from source.'
